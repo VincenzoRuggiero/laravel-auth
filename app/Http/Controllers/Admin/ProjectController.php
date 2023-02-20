e0d914d56c832c200c7b0f8428865500b3cffa67
Vincenzo Ruggiero <vincenzoru@live.it>
2023-02-20 22:28:53 +0100

    Add controller to show all items in a table

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -13,8 +14,9 @@ class ProjectController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        //
+    {   
+        $projects = Project::all();
+        return view('admin.projects.index', compact('projects'));
     }
 
     /**
