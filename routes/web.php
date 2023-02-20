98b4e8ae52de7ba149a4aa9b251f7c872c02d844
Vincenzo Ruggiero <vincenzoru@live.it>
2023-02-20 22:29:19 +0100

    Routes refactor

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\Admin\ProjectController;
+use App\Http\Controllers\Admin\DashboardController as DashboardController;
+use App\Http\Controllers\Admin\ProjectController as ProjectController;
 use App\Http\Controllers\ProfileController;
 use App\Models\Project;
 use Illuminate\Support\Facades\Route;
@@ -20,13 +21,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 //Grouping Admin routes
-Route::prefix('Admin')->name('admin.')->group(function(){
-    Route::resource('projects', ProjectController::class);
+Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function (){
+    Route::get('/dashboard',[DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('/projects', ProjectController::class);
 });
 
 Route::middleware('auth')->group(function () {
