724206e7504d1e48ddcc031f56b004939b505e33
Vincenzo Ruggiero <vincenzoru@live.it>
2023-02-20 17:18:21 +0100

    Create projects migration

--- database/migrations/2023_02_20_160601_create_projects_table.php
+++ database/migrations/2023_02_20_160601_create_projects_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 20)->notnull();
+            $table->string('link', 100)->unique();
+            $table->date('created')->notnull();
             $table->timestamps();
         });
     }
