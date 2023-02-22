f295555d63e89f22c6a34501f7b22876462b7fc0
Vincenzo Ruggiero <vincenzoru@live.it>
2023-02-22 10:19:44 +0100

    Add pagination feature

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -16,7 +16,7 @@ class ProjectController extends Controller
      */
     public function index()
     {   
-        $projects = Project::all();
+        $projects = Project::paginate(20);
         return view('admin.projects.index', compact('projects'));
     }
 
--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -40,6 +40,7 @@
                       @endforeach
                     </tbody>
                   </table>
+                  {{ $projects->links() }}
             </div>
         </div>
    </div>
