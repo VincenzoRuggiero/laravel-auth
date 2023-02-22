ead0d6e317b9ac5e4ec4fea273b1f13a86315a1c
Vincenzo Ruggiero <vincenzoru@live.it>
2023-02-22 18:55:25 +0100

    Fixing '$prev - $next' functionality

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -16,18 +16,22 @@
                 <div class="btn-wrapper d-flex justify-content-between">
                   {{-- Show previous Item --}}
                   <div>
-                     <a class="btn btn-outline-dark" href="{{ route('admin.projects.show', $prev->slug) }}">Prev</a>
+                     <a class="btn btn-outline-dark @if(!isset($prev)) disabled @endisset" href="{{ route('admin.projects.show', $prev->slug ?? '') }}">Prev</a>
                   </div>
 
                   <div class="center-btn">
                      <a href="{{ $project->link }}" class="btn btn-primary">Open Link</a>
                      <a href="{{ route('admin.projects.edit', $project->slug) }}" class="btn btn-success mx-2">Edit</a>
-                     <a href="#" class="btn btn-danger">Delete</a>
+                     <form class="d-inline delete" action="{{ route('admin.projects.destroy', $project->slug) }}" method="POST" class="delete">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger" title="delete">Delete</button>
+                      </form>
                   </div>
 
                   {{-- Show next Item --}}
                   <div>
-                     <a class="btn btn-outline-dark" href="{{ route('admin.projects.show',$next->slug) }}">Next</a>
+                     <a class="btn btn-outline-dark @if(!isset($next)) disabled @endisset" href="{{ route('admin.projects.show', $next->slug ?? '') }}">Next</a>
                   </div>
                 </div>
                 <span>{{ $project->slug }}</span>
@@ -35,4 +39,8 @@
             </div>
         </div>
    </div>
+@endsection
+
+@section('script')
+    @vite('resources/js/confirmDelete.js')
 @endsection
